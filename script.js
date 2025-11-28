639b8b12394959f072317ea98aa3c17a24712930
Nico <nmalaguti@ies-eugeni.cat>
2025-11-28 10:57:43 +0100

    Update script.js
    Removed meteor creation functionality and related constants.

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const starsContainer = document.getElementById('stars');
-const meteorsContainer = document.getElementById('meteors');
-
 const STAR_COUNT = 150;
-const METEOR_INTERVAL = 3000;
 
 // Crear estrellas aleatorias
 for (let i = 0; i < STAR_COUNT; i++) {
@@ -20,23 +17,3 @@ for (let i = 0; i < STAR_COUNT; i++) {
 
   starsContainer.appendChild(star);
 }
-
-// Crear meteoritos periódicamente
-function createMeteor() {
-  const meteor = document.createElement('div');
-  meteor.classList.add('meteor');
-
-  meteor.style.top = `${Math.random() * 50 - 50}px`;
-  meteor.style.left = `${Math.random() * window.innerWidth}px`;
-
-  const duration = Math.random() * 2 + 1;
-  meteor.style.animationDuration = `${duration}s`;
-
-  meteorsContainer.appendChild(meteor);
-
-  setTimeout(() => {
-    meteor.remove();
-  }, duration * 1000);
-}
-
-setInterval(createMeteor, METEOR_INTERVAL);
